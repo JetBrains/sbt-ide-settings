12b5a6abe0a313ba1810473d94bf259123a54e6a
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-10-02 14:17:17 +0400

    add configuration key for importing scalatest tests

--- src/main/scala/sbtidea/Keys.scala
+++ src/main/scala/sbtidea/Keys.scala
@@ -4,11 +4,13 @@ import sbt._
 
 object Keys {
 
-  lazy val exportRunConfiguration = SettingKey[Boolean]("defines whether run configuration of a project should be exported to idea")
-  lazy val artifactRunDependencies   = SettingKey[Seq[String]]("which artifacts should be built before running an idea configuration")
+  lazy val exportRunConfiguration  = SettingKey[Boolean]("defines whether run configuration of a project should be exported to idea")
+  lazy val artifactRunDependencies = SettingKey[Seq[String]]("which artifacts should be built before running an idea configuration")
+  lazy val testInPackage = SettingKey[String]("which package to run tests in in exported configuration")
 
   exportRunConfiguration  := false
   artifactRunDependencies := Seq()
+  testInPackage := ""
 
   val SbtIdea = config("sbtidea")
 }
