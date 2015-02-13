3424859c6c90aca3b04e981ed00922e768842950
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-02-13 16:49:38 +0300

    Refactor AncestorFilter and ideExcludePaths

--- src/main/scala/sbtide/Keys.scala
+++ src/main/scala/sbtide/Keys.scala
@@ -9,12 +9,12 @@ object Keys {
     "Defines whether run configuration of a project should be exported by IDE")
   lazy val ideExportTestConfiguration = SettingKey[Boolean]("ide-export-test-configuration",
     "Defines whether test configuration of a project should be exported by IDE")
-  lazy val ideExcludePaths = SettingKey[Classpath]("ide-exclude-paths",
+  lazy val ideExcludePaths = SettingKey[Seq[File]]("ide-exclude-paths",
     "Defines a list of paths to exclude when indexing project by IDE")
 
   lazy val ideDefaultSettings = Seq(
     ideExportRunConfiguration  := false,
     ideExportTestConfiguration := false,
-    ideExcludePaths := (target.value ** Util.AncestorFilter(sourceManaged.value, resourceManaged.value)).classpath
+    ideExcludePaths := (target.value ** (DirectoryFilter && Util.AncestorFilter(sourceManaged.value, resourceManaged.value))).get
   )
 }
--- src/main/scala/sbtide/Util.scala
+++ src/main/scala/sbtide/Util.scala
@@ -9,15 +9,12 @@ import sbt.{File, FileFilter}
 object Util {
 
   case class AncestorFilter(descendants: File*) extends FileFilter {
-    def accept(file: File): Boolean = {
-      file.isDirectory && (for {
-        descendant <- descendants
-      } yield {
+    def accept(file: File): Boolean =
+      descendants.forall { descendant =>
         var p = descendant
         while (p != null && p != file)
           p = p.getParentFile
         p == null
-      }).forall(_ == true)
-    }
+      }
   }
 }
