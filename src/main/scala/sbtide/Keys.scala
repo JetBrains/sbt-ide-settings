402e67ab674c94fc1d320c4e6db16f35f1e16c04
Pavel Fatin <pavel.fatin@jetbrains.com>
2015-03-05 14:34:44 +0100

    ideBasePackages

--- src/main/scala/sbtide/Keys.scala
+++ src/main/scala/sbtide/Keys.scala
@@ -11,10 +11,13 @@ object Keys {
     "Defines whether test configuration of a project should be exported by IDE")
   lazy val ideExcludedDirectories = SettingKey[Seq[File]]("ide-excluded-directories",
     "Defines a list of paths to exclude when indexing project by IDE")
+  lazy val ideBasePackages = SettingKey[Seq[File]]("ide-base-packages",
+    "Defines a list of base packages for IDE")
 
   lazy val ideDefaultSettings = Seq(
     ideExportRunConfiguration  := false,
     ideExportTestConfiguration := false,
-    ideExcludedDirectories := (target.value ** (DirectoryFilter && Util.AncestorFilter(sourceManaged.value, resourceManaged.value))).get
+    ideExcludedDirectories := (target.value ** (DirectoryFilter && Util.AncestorFilter(sourceManaged.value, resourceManaged.value))).get,
+    ideBasePackages := Seq.empty
   )
 }
