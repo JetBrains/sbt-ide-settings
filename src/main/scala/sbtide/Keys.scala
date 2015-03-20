74c83e69da3d77594eb6ad6c1be1567f44fdeca8
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-03-20 12:21:41 +0300

    Remove currently not working settings

--- src/main/scala/sbtide/Keys.scala
+++ src/main/scala/sbtide/Keys.scala
@@ -4,13 +4,8 @@ import sbt._
 import sbt.Keys._
 
 object Keys {
-
-  lazy val ideExportRunConfiguration  = SettingKey[Boolean]("ide-export-run-configuration",
-    "Defines whether run configuration of a project should be exported by IDE")
-  lazy val ideExportTestConfiguration = SettingKey[Boolean]("ide-export-test-configuration",
-    "Defines whether test configuration of a project should be exported by IDE")
   lazy val ideExcludedDirectories = SettingKey[Seq[File]]("ide-excluded-directories",
-    "Defines a list of paths to exclude when indexing project by IDE")
+    "List of paths to automatically exclude in IDE")
   lazy val ideBasePackages = SettingKey[Seq[String]]("ide-base-packages",
-    "Defines a list of base packages for IDE")
+    "List of packages to be used as prefixes in package chaining")
 }
