667696d313f8499c70277cf159622884053bd942
Justin Kaeser <justin.kaeser@jetbrains.com>
2023-07-04 18:23:20 +0200

    update sbt to 1.9.1

--- src/main/scala/sbtide/ConfigPlugin.scala
+++ src/main/scala/sbtide/ConfigPlugin.scala
@@ -1,8 +1,6 @@
 package sbtide
 
-import sbt._
-
-object ConfigPlugin extends AutoPlugin {
+object ConfigPlugin extends sbt.AutoPlugin {
   override def trigger = noTrigger
   val autoImport = Keys
 }
